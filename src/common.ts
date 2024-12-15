11721b53acef12a5c7bd4fe59274b24efc5773fd
jiang-zhexin <162887873+jiang-zhexin@users.noreply.github.com>
2024-12-15 16:19:07 +0800

    feat: extension to Content-Type

--- src/common.ts
+++ src/common.ts
@@ -1,17 +1,19 @@
 import { encodeHex } from "@std/encoding"
+import { contentType } from "@std/media-types"
 
 import type { MyFile } from "./type"
 
 export async function toMyFile(req: Request): Promise<MyFile> {
     const { pathname } = new URL(req.url)
     const data = await req.bytes()
+    const [_, extname] = pathname.split(".", 2)
 
     return {
         path: pathname,
         data: data,
         size: data.length,
         hash: await Sha256(data),
-        type: req.headers.get("Content-Type") ?? "application/octet-stream",
+        type: req.headers.get("Content-Type") ?? contentType(extname) ?? "application/octet-stream",
     }
 }
 
