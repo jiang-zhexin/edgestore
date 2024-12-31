1e9e4a55458ba8f7aaa39777e6c6a78db26c08fa
jiang-zhexin <162887873+jiang-zhexin@users.noreply.github.com>
2024-12-31 23:30:58 +0800

    feat: more auth info

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono"
 import { bearerAuth } from "hono/bearer-auth"
 import { bodyLimit } from "hono/body-limit"
+import { createMiddleware } from "hono/factory"
 
 import { toMyFile } from "./common"
 import { deleteFiles, getFiles, insertFiles, updateFiles } from "./db"
@@ -25,11 +26,19 @@ app.get(
     }
 )
 
+const BearerAuthMiddleware = createMiddleware(
+    bearerAuth({
+        realm: "Your Token",
+        verifyToken: (token, c) => token === c.env.token,
+        noAuthenticationHeaderMessage: (c) => c.text("Unauthorized", 401),
+        invalidAuthenticationHeaderMessage: (c) => c.text("Unauthorized", 401),
+        invalidTokenMessage: (c) => c.text("Forbidden", 403),
+    })
+)
+
 app.put(
     "*",
-    bearerAuth({
-        verifyToken: (token, c) => token === c.env.token,
-    }),
+    BearerAuthMiddleware,
     bodyLimit({
         maxSize: 25 * 1024 * 1024,
         onError: (c) => {
@@ -53,26 +62,20 @@ app.put(
     }
 )
 
-app.delete(
-    "*",
-    bearerAuth({
-        verifyToken: (token, c) => token === c.env.token,
-    }),
-    async (c) => {
-        const existFiles = await getFiles(c.env)
-        const removeFiles = existFiles.filter((f) => f.path === c.req.path)
+app.delete("*", BearerAuthMiddleware, async (c) => {
+    const existFiles = await getFiles(c.env)
+    const removeFiles = existFiles.filter((f) => f.path === c.req.path)
 
-        if (removeFiles.length === 0) {
-            return c.text("404 Not Found", 404)
-        }
-
-        if (c.req.query("lazy") === "false") {
-            const syncFiles = existFiles.filter((f) => f.path !== c.req.path)
-            await Sync(c.env, syncFiles)
-        }
-        c.executionCtx.waitUntil(deleteFiles(c.env, ...removeFiles))
-        return c.newResponse(null, 204)
+    if (removeFiles.length === 0) {
+        return c.text("404 Not Found", 404)
     }
-)
+
+    if (c.req.query("lazy") === "false") {
+        const syncFiles = existFiles.filter((f) => f.path !== c.req.path)
+        await Sync(c.env, syncFiles)
+    }
+    c.executionCtx.waitUntil(deleteFiles(c.env, ...removeFiles))
+    return c.newResponse(null, 204)
+})
 
 export default app
