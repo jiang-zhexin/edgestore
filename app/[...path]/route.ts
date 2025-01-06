11ebfb2bbb0a0eee9dbd10b762973a2598a290b5
jiang-zhexin <162887873+jiang-zhexin@users.noreply.github.com>
2025-01-06 13:57:53 +0800

    fix: env, ctx

--- app/[...path]/route.ts
+++ app/[...path]/route.ts
@@ -5,13 +5,15 @@ import { cors } from "hono/cors"
 import { createMiddleware } from "hono/factory"
 import { handle } from "hono/vercel"
 
+import { getRequestContext } from "@cloudflare/next-on-pages"
+
 import { toMyFile } from "@/utils/common"
 import { deleteFiles, getFiles, insertFiles, updateFiles } from "@/utils/db"
 import { Sync } from "@/utils/store"
 
 export const runtime = "edge"
 
-const app = new Hono<{ Bindings: CloudflareEnv }>()
+const app = new Hono()
 
 app.get(
     "*",
@@ -25,7 +27,9 @@ app.get(
         maxAge: 604800,
     }),
     async (c) => {
-        const response = await c.env.store.fetch(c.req.raw)
+        const { env } = getRequestContext()
+
+        const response = await env.store.fetch(c.req.url)
         return new Response(response.body, response)
     }
 )
@@ -33,7 +37,7 @@ app.get(
 const BearerAuthMiddleware = createMiddleware(
     bearerAuth({
         realm: "Your Token",
-        verifyToken: (token, c) => token === c.env.token,
+        verifyToken: (token, c) => token === getRequestContext().env.token,
         noAuthenticationHeaderMessage: (c) => c.text("401 Unauthorized", 401),
         invalidAuthenticationHeaderMessage: (c) => c.text("401 Unauthorized", 401),
         invalidTokenMessage: (c) => c.text("403 Forbidden", 403),
@@ -50,24 +54,28 @@ app.put(
         },
     }),
     async (c) => {
-        const [newFile, existFiles] = await Promise.all([toMyFile(c.req.raw), getFiles(c.env)])
+        const { env, ctx } = getRequestContext()
+
+        const [newFile, existFiles] = await Promise.all([toMyFile(c.req.raw), getFiles(env)])
         const syncFiles = existFiles.filter((f) => f.path !== newFile.path).concat(newFile)
 
-        await Sync(c.env, syncFiles)
+        await Sync(env, syncFiles)
 
         c.header("Content-Location", c.req.url)
         if (syncFiles.length > existFiles.length) {
-            c.executionCtx.waitUntil(insertFiles(c.env, newFile))
+            ctx.waitUntil(insertFiles(env, newFile))
             c.header("Location", c.req.url)
             return c.text("201 Created", 201)
         }
-        c.executionCtx.waitUntil(updateFiles(c.env, newFile))
+        ctx.waitUntil(updateFiles(env, newFile))
         return c.newResponse(null, 204)
     }
 )
 
 app.delete("*", BearerAuthMiddleware, async (c) => {
-    const existFiles = await getFiles(c.env)
+    const { env, ctx } = getRequestContext()
+
+    const existFiles = await getFiles(env)
     const removeFiles = existFiles.filter((f) => f.path === c.req.path)
 
     if (removeFiles.length === 0) {
@@ -76,9 +84,9 @@ app.delete("*", BearerAuthMiddleware, async (c) => {
 
     if (c.req.query("lazy") === "false") {
         const syncFiles = existFiles.filter((f) => f.path !== c.req.path)
-        await Sync(c.env, syncFiles)
+        await Sync(env, syncFiles)
     }
-    c.executionCtx.waitUntil(deleteFiles(c.env, ...removeFiles))
+    ctx.waitUntil(deleteFiles(env, ...removeFiles))
     return c.newResponse(null, 204)
 })
 
